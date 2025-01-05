22a1629ea0b03c3b761dd64a33bb43523031aef9
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2025-01-05 20:16:17 +0530

    Added further Model validations   - specified valid values by using enums   - reflected changes in views event/update & event/add

--- src/model/Event.ts
+++ src/model/Event.ts
@@ -4,11 +4,32 @@ const eventSchema = new mongoose.Schema({
   name: { type: String, required: true },
   dateTime: { type: Date, required: true },
   memberCount: { type: Number, required: true },
-  status: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["UPCOMING", "ONGOING", "CLOSED"],
+      message: "{VALUE} is not supported",
+    },
+  },
   fee: { type: Number, required: true },
   rules: { type: [String], required: true },
-  category: { type: String, required: true },
-  gender: { type: String, required: true },
+  category: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["TECH", "NONTECH", "CULTURAL", "SPORTS", "SPECIAL"],
+      message: "{VALUE} is not supported",
+    },
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["ALL", "BOYS", "GIRLS"],
+      message: "{VALUE} is not supported",
+    },
+  },
   imageURL: { type: String, required: true },
 });
 
